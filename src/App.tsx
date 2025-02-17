71e8810bc996b4105a7c4e16b778e844b68d7c2b
guitar6029 <jsbusiness305@gmail.com>
2025-02-17 14:20:51 -0500

    test

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,41 @@ import { getPosts } from '../src/api/fetchPosts';
 import type { Post } from '../src/types/Post';
 import Card from '../src/components/Card';
 
+const myObjectExample = {
+  id: 123,
+  name: "James Smith",
+  job: "Programmer"
+}
+
+
+type MyObjectProps = {
+  id: number,
+  name: string,
+  job?: string
+
+}
+
+function MyObject({...props}: MyObjectProps){
+  return (
+    <div>
+      <span>{props.id}</span><br />
+      <span>{props.name}</span><br />
+      {props.job && <span>{props.job}</span>}
+    </div>
+  )
+}
+
+type ButtonProps = {
+  clickedHello: () => void;
+}
+
+function Button({clickedHello} : ButtonProps) {
+  return <button onClick={clickedHello}>Hello</button>
+}
+
 function App() {
   const [posts, setPosts] = useState<Post[]>([]);
-
+  const [hello, toggleHello] = useState<boolean>(false)
   const [loadingPosts, setLoadingPosts] = useState<boolean>(true);
 
   // on mounted
@@ -31,10 +63,17 @@ function App() {
     return <div>Loading Posts...</div>;
   }
 
+  const handleClickedHello = () => {
+    console.log("Current property: ", hello)
+    toggleHello(!hello)
+  }
+
   return (
     <div>
-      <h1>Posts</h1>
-      <ul>
+      <h1>Posts Test <Button clickedHello={handleClickedHello} /></h1>
+      <span>{hello ? 'True' : 'False'}</span>
+      <MyObject {...myObjectExample} />
+      <ol>
         {posts?.map((post, index: number) => {
           return (
             <li key={index}>
@@ -42,8 +81,8 @@ function App() {
             </li>
           );
         })}
-      </ul>
-      <Card post={posts[0]}/>
+      </ol>
+      <Card post={posts[0]} />
     </div>
   );
 }
